b57b6d496416f79a5eefa678da1fce1fa3204763
Draakaap <draakaapdev@gmail.com>
2016-11-22 15:57:23 +0100

    A working JSON list being parsed by Angular.

--- MySandBox/Controllers/HelloController.cs
+++ MySandBox/Controllers/HelloController.cs
@@ -3,16 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace MySandBox.Controllers
 {
     public class HelloController : Controller
     {
+        public class person
+        {
+            public string name { get; set; }
+            public DateTime birthdate { get; set; }
+            public bool married { get; set; }
+        }
         // GET: Hello
         public ActionResult Index()
         {
-            string Output = "Hello World";
-            return View(Output as Object);
+            var Output = 
+                new List<person>()
+                {
+                    new person(){
+                        name = "Hello World",
+                        birthdate = DateTime.Now,
+                        married = false
+                    },
+                    new person(){
+                        name = "Arie Ribbens",
+                        birthdate = DateTime.Now,
+                        married = true
+                    },
+                    new person(){
+                        name = "Rob Williams",
+                        birthdate = DateTime.Now,
+                        married = true
+                    }
+
+                }.ToList();
+            return View("Index","",JsonConvert.SerializeObject( Output));
         }
     }
 }
